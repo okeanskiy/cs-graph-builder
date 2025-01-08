c8e3b0408e4b9a59fdcc925035401360e311cc3d
Adam Cunningham <57463819+okeanskiy@users.noreply.github.com>
2025-01-08 12:43:05 -0500

    rename project files

--- run.ps1
+++ run.ps1
@@ -5,10 +5,10 @@ param(
 
 if ($Build) {
     Write-Host "[run.ps1] Building the project..."
-    dotnet build ./Strazh/Strazh.csproj -c Release -o ./app
+    dotnet build ./cs-graph-builder/cs-graph-builder.csproj -c Release -o ./app
 }
 
 Write-Host "[run.ps1] Running the project..."
 
 # Run the project
-dotnet ./app/Strazh.dll -c neo4j:neo4j:strazhstrazh -p ./Strazh/Strazh.csproj
+dotnet ./app/cs-graph-builder.dll -c neo4j:neo4j:strazhstrazh -p ./cs-graph-builder/cs-graph-builder.csproj
