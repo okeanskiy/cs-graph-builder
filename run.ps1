289edd6bb8438ecbcad8f75a8384ae6fc9751ef6
Adam Cunningham <57463819+okeanskiy@users.noreply.github.com>
2025-01-08 12:51:23 -0500

    Stop script if build fails

--- run.ps1
+++ run.ps1
@@ -6,6 +6,10 @@ param(
 if ($Build) {
     Write-Host "[run.ps1] Building the project..."
     dotnet build ./cs-graph-builder/cs-graph-builder.csproj -c Release -o ./app
+    if ($LASTEXITCODE -ne 0) {
+        Write-Host "[run.ps1] Build failed. Exiting..."
+        exit $LASTEXITCODE
+    }
 }
 
 Write-Host "[run.ps1] Running the project..."
